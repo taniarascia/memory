f03b521540479dbb5fe1c4d26c05d864f5db2502
Tania Rascia <taniarascia@gmail.com>
2018-02-07 15:22:52 -0600

    Update script.original.js

--- js/script.original.js
+++ js/script.original.js
@@ -1,4 +1,3 @@
-// Card data
 const cardsArray = [{
     'name': 'shell',
     'img': 'img/blueshell.png',
@@ -49,19 +48,20 @@ const cardsArray = [{
   },
 ];
 
-// Duplicate array to create a match for each card
 let gameGrid = cardsArray.concat(cardsArray);
-
-// Randomize game grid on each load
 gameGrid.sort(() => 0.5 - Math.random());
 
-// Define app root and outer grid
+let firstGuess = '';
+let secondGuess = '';
+let count = 0;
+let previousTarget = null;
+let delay = 1200;
+
 const game = document.getElementById('game');
 const grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
-// Set each card item to a div element and append to the DOM
 gameGrid.forEach(item => {
   const card = document.createElement('div');
   card.classList.add('card');
@@ -79,29 +79,6 @@ gameGrid.forEach(item => {
   card.appendChild(back);
 });
 
-let firstGuess = '';
-let secondGuess = '';
-let count = 0;
-let previousTarget = null;
-let clickDisabled = false;
-let delay = 1200;
-
-// Reset on success and fail
-const resetGuesses = () => {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-};
-
-// Remove selected CSS
-const deselect = () => {
-  var selected = document.querySelectorAll('.selected');
-  selected.forEach(card => {
-    card.classList.remove('selected');
-  });
-}
-
-// Add match CSS
 const match = () => {
   var selected = document.querySelectorAll('.selected');
   selected.forEach(card => {
@@ -109,45 +86,45 @@ const match = () => {
   });
 }
 
-// Click event 
+const resetGuesses = () => {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(card => {
+    card.classList.remove('selected');
+  });
+};
+
 grid.addEventListener('click', function (event) {
   let clicked = event.target;
-  // Disallow clicking of already matched elements, parent element, and third click
-  if (clicked.parentNode.classList.contains('match') ||
-    clicked.nodeName === 'SECTION' ||
-    clickDisabled) {
-    return;
-  }
-  // Only count first two clicks
+if (
+  clicked.nodeName === 'SECTION' ||
+  clicked === previousTarget ||
+  clicked.parentNode.classList.contains('match')
+) {
+  return;
+}
   if (count < 2) {
     count++;
-    // Select the first two guesses
     if (count === 1) {
       firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
       clicked.parentNode.classList.add('selected');
-    } else if (count === 2) {
+    } else {
       secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
       clicked.parentNode.classList.add('selected');
     }
-    // If first and second guesses have been made
     if (firstGuess !== '' && secondGuess !== '') {
-      // If the guesses aren't the same element but have the same name
-      if (firstGuess === secondGuess && event.target !== previousTarget) {
+      if (firstGuess === secondGuess) {
         setTimeout(match, delay);
-        setTimeout(deselect, delay);
         setTimeout(resetGuesses, delay);
       } else {
-        setTimeout(deselect, delay);
         setTimeout(resetGuesses, delay);
       }
     }
-    // Make sure both targets aren't the same element
-    previousTarget = event.target;
-  } else {
-    // Disable third click until timeouts complete
-    clickDisabled = true;
-    setTimeout(() => {
-      clickDisabled = false;
-    }, delay);
+    previousTarget = clicked;
   }
 });
