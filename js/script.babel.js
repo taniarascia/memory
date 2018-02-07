434bd6c1886604650fa19282ab94ecb47af59183
Tania Rascia <taniarascia@gmail.com>
2018-02-07 15:23:31 -0600

    Update script.babel.js

--- js/script.babel.js
+++ js/script.babel.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Card data
 var cardsArray = [{
   'name': 'shell',
   'img': 'img/blueshell.png'
@@ -39,21 +38,22 @@ var cardsArray = [{
   'img': 'img/goomba.png'
 }];
 
-// Duplicate array to create a match for each card
 var gameGrid = cardsArray.concat(cardsArray);
-
-// Randomize game grid on each load
 gameGrid.sort(function () {
   return 0.5 - Math.random();
 });
 
-// Define app root and outer grid
+var firstGuess = '';
+var secondGuess = '';
+var count = 0;
+var previousTarget = null;
+var delay = 1200;
+
 var game = document.getElementById('game');
 var grid = document.createElement('section');
 grid.setAttribute('class', 'grid');
 game.appendChild(grid);
 
-// Set each card item to a div element and append to the DOM
 gameGrid.forEach(function (item) {
   var card = document.createElement('div');
   card.classList.add('card');
@@ -71,29 +71,6 @@ gameGrid.forEach(function (item) {
   card.appendChild(back);
 });
 
-var firstGuess = '';
-var secondGuess = '';
-var count = 0;
-var previousTarget = null;
-var clickDisabled = false;
-var delay = 1200;
-
-// Reset on success and fail
-var resetGuesses = function resetGuesses() {
-  firstGuess = '';
-  secondGuess = '';
-  count = 0;
-};
-
-// Remove selected CSS
-var deselect = function deselect() {
-  var selected = document.querySelectorAll('.selected');
-  selected.forEach(function (card) {
-    card.classList.remove('selected');
-  });
-};
-
-// Add match CSS
 var match = function match() {
   var selected = document.querySelectorAll('.selected');
   selected.forEach(function (card) {
@@ -101,43 +78,41 @@ var match = function match() {
   });
 };
 
-// Click event 
+var resetGuesses = function resetGuesses() {
+  firstGuess = '';
+  secondGuess = '';
+  count = 0;
+
+  var selected = document.querySelectorAll('.selected');
+  selected.forEach(function (card) {
+    card.classList.remove('selected');
+  });
+};
+
 grid.addEventListener('click', function (event) {
   var clicked = event.target;
-  // Disallow clicking of already matched elements, parent element, and third click
-  if (clicked.parentNode.classList.contains('match') || clicked.nodeName === 'SECTION' || clickDisabled) {
+  if (clicked.nodeName === 'SECTION' || clicked === previousTarget || clicked.parentNode.classList.contains('match')) {
     return;
   }
-  // Only count first two clicks
   if (count < 2) {
     count++;
-    // Select the first two guesses
     if (count === 1) {
       firstGuess = clicked.parentNode.dataset.name;
+      console.log(firstGuess);
       clicked.parentNode.classList.add('selected');
-    } else if (count === 2) {
+    } else {
       secondGuess = clicked.parentNode.dataset.name;
+      console.log(secondGuess);
       clicked.parentNode.classList.add('selected');
     }
-    // If first and second guesses have been made
     if (firstGuess !== '' && secondGuess !== '') {
-      // If the guesses aren't the same element but have the same name
-      if (firstGuess === secondGuess && event.target !== previousTarget) {
+      if (firstGuess === secondGuess) {
         setTimeout(match, delay);
-        setTimeout(deselect, delay);
         setTimeout(resetGuesses, delay);
       } else {
-        setTimeout(deselect, delay);
         setTimeout(resetGuesses, delay);
       }
     }
-    // Make sure both targets aren't the same element
-    previousTarget = event.target;
-  } else {
-    // Disable third click until timeouts complete
-    clickDisabled = true;
-    setTimeout(function () {
-      clickDisabled = false;
-    }, delay);
+    previousTarget = clicked;
   }
 });
